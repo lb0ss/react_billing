c6aeb1fa41c493e13a9cdd2e6397a8bb4683a891
blin <blin@occourts.org>
2021-11-15 08:50:34 -0800

    completed basic components

---
+++ src/components/Card.js
@@ -0,0 +1,6 @@
+function Card(props) {
+    const classes = 'card ' + props.className;
+    return <div className={classes}>{props.children}</div>   // content in between the opening and closing tag is passed in via props.children
+}
+
+export default Card;
