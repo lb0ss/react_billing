ddab29452d895e7e80c66cb60b96970457443f4a
blin <blin@occourts.org>
2021-11-28 15:26:21 -0800

    handle user input with state and two-way binding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses
  from "./components/Expenses/Expenses";
 
@@ -35,7 +36,7 @@ const App = () => {
 
   return (
     <div>
-          <h2>Let's get started</h2>
+          <NewExpense />
           <Expenses items={expenses}/>
     </div>
   );
